84dc60d0e7344b375f0e4d9ce0f28300cb9ab472
Luis Abreu <luisandre_69@hotmail.com>
2019-09-24 12:31:17 +0100

    #4

---
+++ operators.py
@@ -0,0 +1,33 @@
+#%% Mahematical operators
+print(5/2)
+print(5//2) #floor division
+print(7%2) #module
+x=2
+print(x)
+x+=1
+print(x)
+#%% Relational operators
+print(8!=9)
+print(10>=5)
+
+#%% Logical operators
+print((5>4) and (9<8))
+print((5>4) or (9<8))
+
+#%% Membership operators
+print(5 in [1,8,5,9])
+print(4 not in [5,6,8])
+
+#%% Identity operator
+x=4
+y=4
+print(x is y)
+print("id(x): ",id(x))
+print("id(y): ",id(y))
+
+#%% Operator precedence
+print(2+4*2)
+print((2+4)*2)
+print(2*4**2)
+
+#%%
