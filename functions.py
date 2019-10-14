7974859537982ed3393754e37a31af1bfe64f8cf
Luis Abreu <luisandre_69@hotmail.com>
2019-10-14 20:08:03 +0100

    #8

---
+++ functions.py
@@ -0,0 +1,27 @@
+### python built-in functions
+# abs()           dict()          help()          min()       setattr)()
+# all()           dir()           hex()           next()      slice()
+# any()           divmod()        id()            object()    sorted()
+# ascii()         enumerate()     input()         oct()       staticmethor()
+# bin()           eval()          int()           open()      str()
+# bool()          exec()          isinstance()    ord()       sum()
+# bytearray()     filter()        issubclass()    pow()       super()
+# bytes()         float()         iter()          print()     tuple()
+# callable()      format()        len()           property()  type()
+# chr()           frozenset()     list()          range()     vars()
+# classmethod()   qetattr()       locals()        repr()      zip()
+# compile()       globals()       map()           reversed()  __import__()
+#%%
+def area_cirle(radius):
+    pi=22/7
+    area=pi*radius**2
+    return area
+
+radius=5
+area_cirle(radius)
+area_cirle(10)
+
+#%%
+abs(-4)
+len([1,5,8,9])
+int(4.5)
