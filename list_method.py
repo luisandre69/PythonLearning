bc504b588e9170ae06f8e9dd1f24ed9e95ebff67
Luis Abreu <luisandre_69@hotmail.com>
2019-10-17 21:41:32 +0100

    lists

---
+++ list_method.py
@@ -0,0 +1,21 @@
+#%% List.insert(i,x)
+L=[1,2,3]
+L.insert(0,100)
+L   #[100,1,2,3]
+#%% List.append(x)
+L=[100,1,2,3]
+L.append(500)
+L   #[100,1,2,3,500]
+#%% List.remove(x)
+L=[100,1,2,3,500]
+L.remove(500)
+L   #[100,1,2,3]
+#%% List.pop([i])
+L=[100,1,2,3]
+L.pop(1)
+L   #[100,2,3] removes i posiiton
+#%% List.pop()
+L=[100,1,2,3]
+L.pop()
+L   #[100,1,2] removes last element
+#%%
